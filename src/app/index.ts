1d6a0f337c9539cb0933ab13d37dda451d6f4672
about-yy <work.yamaoka@gmail.com>
2021-02-02 23:36:10 +0900

    routerを使用するように変更

--- src/app/index.ts
+++ src/app/index.ts
@@ -7,9 +7,9 @@ class Server{
 
     constructor(){
         this.app = express();
-        this.app.get("/", (request: express.Request,  response: express.Response)=>{
-            response.send("hello");
-        });
+        this.middleware();
+        this.route();
+        this.errorHandle();
         this.app.listen(process.env.PORT);
     }
 
@@ -21,7 +21,12 @@ class Server{
 
     // ルーティング設定
     private route(){
-    
+        const indexRouter = express.Router();
+        indexRouter.get("/", (request: express.Request,  response: express.Response)=>{
+            response.send("hello");
+        });
+        
+        this.app.use(indexRouter);
     }
 
     // エラーハンドリング設定
