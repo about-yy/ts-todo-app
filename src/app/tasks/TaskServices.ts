5ede936da1055c7fd944d8a3def6daf5089b751b
about-yy <work.yamaoka@gmail.com>
2021-02-06 18:46:29 +0900

    サービスを作成

--- src/app/tasks/TaskServices.ts
+++ src/app/tasks/TaskServices.ts
@@ -3,18 +3,39 @@ import { CrudService } from "../common/CrudService";
 export class TaskService implements CrudService{
     constructor(){
     }
+
+    /**
+     * タスク登録
+     */
     public create() {
+        
         throw new Error("Method not implemented.");
     }
+    
+    /**
+     * タスク一覧
+     */
     public list() {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * タスク詳細
+     */
     public find() {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * タスク更新
+     */
     public update() {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * タスク削除
+     */
     public delete() {
         throw new Error("Method not implemented.");
     }
