483f421ddd0a594468ad46f557f03f7154a18fa2
about-yy <work.yamaoka@gmail.com>
2021-02-27 01:07:17 +0900

    初期化処理と終了処理を分離

--- src/test/TaskRepository.test.ts
+++ src/test/TaskRepository.test.ts
@@ -2,8 +2,29 @@ import { Task } from "../app/tasks/Task";
 import { TaskRepository } from "../app/tasks/TaskCrudRepository";
 
 const repository = new TaskRepository();
+beforeAll(()=>{
+    repository.deleteAll();
+
+    repository.create(new Task(
+        "find task",
+        Task.STATE_NOTE,
+        new Date()
+    ));
+    
+    repository.create(new Task(
+        "update task",
+        Task.STATE_NOTE,
+        new Date()
+    ));
+})
+
+afterAll(()=>{
+    repository.deleteAll();
+})
 
 describe("Task Repository Tests", ()=>{
+
+   
     test("create new task", async ()=>{
         const task = new Task(
             "test task",
@@ -14,12 +35,12 @@ describe("Task Repository Tests", ()=>{
     })
 
     test("find created task", async ()=>{
-        expect((await repository.find(24)).name).toBe("test task"||"test task updated");
+        expect((await repository.find(1)).name).toBe("find task");
     })
 
     test("update find task", async()=>{
-        const task = await repository.find(22);
-        task.name = "test task updated";
+        const task = await repository.find(2);
+        task.name = "updated task";
         const updated = await repository.update(task);
         expect(updated.id).toBe(task.id);
     })
@@ -29,20 +50,11 @@ describe("Task Repository Tests", ()=>{
         const limit = 5;
         expect((await repository.list(offset, limit)) instanceof Array).toBe(true);
         expect((await repository.list(offset, limit))[0] instanceof Task).toBe(true);
-        
     })
 
     test("delete find task", async()=>{
-        const task = await repository.find(21);
-        expect((await repository.delete(task))).toBe(21);
+        const task = await repository.find(1);
+        expect((await repository.delete(task))).toBe(1);
     })
 
-    test("delete all", async()=>{
-        expect(await repository.deleteAll()).toBe(true);
-    })
 });
-
-
-
-
-
