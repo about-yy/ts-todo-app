9d335f32ee37ea43aae52b1c0e2a6eb6ffc2c14b
about-yy <work.yamaoka@gmail.com>
2021-02-27 02:32:26 +0900

    型定義を仮決め

--- src/app/common/ICrudService.ts
+++ src/app/common/ICrudService.ts
@@ -1,7 +1,7 @@
-export interface ICrudService<T, S>{
-    create(): ;
-    list(): any;
-    find(): any;
-    update(): any;
-    delete(): any;
+export interface ICrudService<T, S, U>{
+    create(form: S): Promise<U>;
+    list(offset: number, limit:number ): Promise<Array<U>>;
+    find(id: T): Promise<U>;
+    update(id: T, from: S): Promise<U>;
+    delete(id: T): Promise<boolean>;
 }
