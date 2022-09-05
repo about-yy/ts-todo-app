f02a1b36606796f5845bc9160b367dd3759b1de1
Yamada <coder.yamada.taro@gmail.com>
2022-09-06 01:37:20 +0900

    ログイン処理を仮置き

--- backend/app/src/index.ts
+++ backend/app/src/index.ts
@@ -1,10 +1,2 @@
-import express from "express";
-const app: express.Express = express();
-
-app.listen(process.env.PORT, ()=>{
-    console.log("Start on port "+process.env.PORT);
-});
-
-app.get('/test', (req: express.Request, res: express.Response)=>{
-    res.send("hello from index");
-});
+import App from "./app/App";
+new App().run();
