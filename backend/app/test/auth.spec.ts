89c5f69e959853f411e2c8bdc2ba718944697ea7
about-yy <work.yamaoka@gmail.com>
2022-10-16 01:38:00 +0900

    supertest を導入

---
+++ backend/app/test/auth.spec.ts
@@ -0,0 +1,21 @@
+import { expect } from "chai";
+import supertest from "supertest";
+import index from "../src/index";
+const app =  supertest(index);
+describe("supertest sample", ()=>{
+    it("get isLogined", async ()=>{
+        const response = await app.get("/auth/isLogined");
+        expect(response.status).to.equal(200);
+        expect(response.text).to.equal("isLogined api");
+    });
+    it("post login", async ()=>{
+        const response = await app.post("/auth/login");
+        expect(response.status).to.equal(200);
+        expect(response.text).to.equal("login api");
+    });
+    it("post logout", async ()=>{
+        const response = await app.post("/auth/logout");
+        expect(response.status).to.equal(200);
+        expect(response.text).to.equal("logout api");
+    });
+});
