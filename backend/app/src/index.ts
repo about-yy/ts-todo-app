39af6279e9db2d339203c3f8119340fcf5fadd4e
Yamada <coder.yamada.taro@gmail.com>
2022-12-03 01:12:02 +0900

    log4jsを追加

--- backend/app/src/index.ts
+++ backend/app/src/index.ts
@@ -2,4 +2,16 @@ import App from "./app/App";
 import express from "express";
 const expressApp = express();
 new App(expressApp).run();
+
+import { configure, getLogger, Log4js } from "log4js";
+configure({
+    appenders: {
+      app: { type: "file", filename: "log.txt" },
+    },
+    categories: {
+      default: { appenders: ["app"], level: "debug" },
+    },
+  });
+const logger = getLogger();
+logger.debug('Hello World!!!!');
 export default expressApp;
