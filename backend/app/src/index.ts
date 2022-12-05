e3e14f1e4c018cce90c8e95f8e6b4855ea9a5154
Yamada <coder.yamada.taro@gmail.com>
2022-12-06 01:41:56 +0900

    ログ処理用のクラスを作成

--- backend/app/src/index.ts
+++ backend/app/src/index.ts
@@ -2,9 +2,4 @@ import App from "./app/App";
 import express from "express";
 const expressApp = express();
 new App(expressApp).run();
-
-import { configure, getLogger, Log4js } from "log4js";
-configure("./config/log4js.config");
-const logger = getLogger();
-logger.debug('Hello World!!!!');
 export default expressApp;
