d6b3ac5b888eb1387595812afe8894ede57bce44
about-yy <work.yamaoka@gmail.com>
2023-02-06 13:59:10 +0900

    登録済みユーザのエラー処理を追加

--- backend/app/src/auth/AuthRepository.ts
+++ backend/app/src/auth/AuthRepository.ts
@@ -1,16 +1,21 @@
 import User from "../user/User";
 import { PrismaClient } from "@prisma/client";
+import PrismaUtils from "../common/PrismaUtils";
 
 export default class AuthRepository {
     async findUser(email: string, password: string): Promise<User>{
-        const prisma = new PrismaClient();
-        const found = await prisma.user.findFirstOrThrow({
-            where: {
-                email: email,
-                password: password
-            }
-        });
-        const user = new User(found.id.toLocaleString(), found.email, found.name);
-        return user;
+        try {            
+            const prisma = new PrismaClient();
+            const found = await prisma.user.findFirstOrThrow({
+                where: {
+                    email: email,
+                    password: password
+                }
+            });
+            const user = new User(found.id.toLocaleString(), found.email, found.name);
+            return user;
+        } catch (error) {
+            throw PrismaUtils.getHttpException(error);
+        }
     }
 }
