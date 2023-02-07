3b73e26c49e101b5cbf8b590743fd6516e2b8932
about-yy <work.yamaoka@gmail.com>
2023-02-07 17:26:41 +0900

    テストコードを追加

--- backend/app/src/auth/AuthService.ts
+++ backend/app/src/auth/AuthService.ts
@@ -5,18 +5,14 @@ import { HttpsError } from "../common/http-error";
 
 export default class AuthService {
     async login(email: string, password: string): Promise<User>{
-        try {
-            const repository = new AuthRepository();
-            const user = repository.findUser(email);
-            const passwordIsEqual = compareSync(password, (await user).hashedPassword.toString());
+        const repository = new AuthRepository();
+        const user = repository.findUser(email);
+        const passwordIsEqual = compareSync(password, (await user).hashedPassword.toString());
 
-            if(passwordIsEqual){
-                return user;
-            } else {
-                throw new HttpsError('unauthenticated', "password or email is not correct");
-            }
-        } catch (error) {
-            throw new Error("login failed");
+        if(passwordIsEqual){
+            return user;
+        } else {
+            throw new HttpsError('unauthenticated', "password or email is not correct");
         }
     }
 }
