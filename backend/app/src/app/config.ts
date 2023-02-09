ad69ce07fd27879cc5aefaeff8dc11e882ca079b
about-yy <work.yamaoka@gmail.com>
2023-02-09 14:10:36 +0900

    config値を修正

--- backend/app/src/app/config.ts
+++ backend/app/src/app/config.ts
@@ -1,5 +1,5 @@
 const keys = {
-    port: process.env.PORT || '3001',
+    port: process.env.PORT,
     SECRET_KEY: process.env.SECRET_KEY || 'secret_key',
     EXPIRES_IN: process.env.EXPIRES_IN || 60*10,
 }
