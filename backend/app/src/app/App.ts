1d038865e37b4585e42c3d1aa6246021f3f4c215
about-yy <work.yamaoka@gmail.com>
2023-02-09 13:57:03 +0900

    ミドルウェアを作成

--- backend/app/src/app/App.ts
+++ backend/app/src/app/App.ts
@@ -5,6 +5,7 @@ import 'express-async-errors';
 import ErrorHandler from "./ErrorHandler";
 import { HttpsError } from "../common/http-error";
 import config from "./config";
+import Authenticator from "../common/Authenticater";
 export default class App {
     private _express: express.Express;
     constructor(express: express.Express){
@@ -14,13 +15,14 @@ export default class App {
         this._express.listen(config.port, ()=>{
             console.log("Start on port "+config.port);
         });
+        
+        const authenticator = new Authenticator();
 
         this._express.use(express.json());
         this._express.use(express.urlencoded({extended: true}));
-
         this._express.post("/auth/login", new AuthController().login );
         this._express.post("/auth/logout", new AuthController().logout );
-        this._express.get("/auth/isLogined", new AuthController().isLogined );
+        this._express.get("/auth/isLogined", authenticator.authenticate, new AuthController().isLogined );
         this._express.post("/user/regist", new UserController().regist)
         this._express.get("/error", async (req: Request, res: Response, next: NextFunction)=>{
             throw new HttpsError("internal", "this is error test page");
