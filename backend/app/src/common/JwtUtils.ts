182ff9d074a79815d3d03954713bc6d11eeb49ff
about-yy <work.yamaoka@gmail.com>
2023-02-15 15:02:24 +0900

    引数を追加

--- backend/app/src/common/JwtUtils.ts
+++ backend/app/src/common/JwtUtils.ts
@@ -11,9 +11,25 @@ export default class JwtUtils {
         }
     }
 
-    static async getUserId(req: Request): Promise<number>{
-        const token = await this.getJwtFromRequest(req);
+    static async getUserId(token: string): Promise<number>;
+    static async getUserId(req: Request): Promise<number>;
+    static async getUserId(value: string | Request): Promise<number>{
+        if(typeof value == "string"){
+            return this.getUserIdByToken(value);
+        } else {
+            return this.getUserIdByRequest(value);
+        }
+    }
+
+    private static async getUserIdByToken(token: string){
         const payload = jwt.decode(token) as jwt.JwtPayload;
         return payload.userId;
     }
+
+    private static async getUserIdByRequest(req: Request){
+        const token = await this.getJwtFromRequest(req);    
+        const payload = jwt.decode(token) as jwt.JwtPayload;
+        return payload.userId;
+    }
+
 }
