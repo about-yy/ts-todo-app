80fbb7fa8482a36cab64336ce4db9381a4b3fb1e
about-yy <work.yamaoka@gmail.com>
2023-03-02 09:12:06 +0900

    HTMLを作成

--- frontend/app/src/components/SignUpPage.vue
+++ frontend/app/src/components/SignUpPage.vue
@@ -1,5 +1,13 @@
 <template>
-    <p>hello from signup page</p> 
+    <v-card class="signup-form" title="ユーザ登録|TS TODO APP">
+        <v-container>
+            <v-text-field id="email" clearable class="text-input" label="メールアドレス"></v-text-field>
+            <v-text-field id="username" clearable class="text-input" label="ユーザー名"></v-text-field>
+            <v-text-field id="password" clearable class="text-input" label="パスワード"></v-text-field>
+            <v-text-field id="password_confirm" clearable class="text-input" label="パスワード（確認用）"></v-text-field>
+        </v-container>
+        <v-btn type="submit" class="submit-btn" variant="flat" color="primary">登録</v-btn>
+    </v-card>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
@@ -10,3 +18,13 @@ export default defineComponent({
     },
 })
 </script>
+<style scoped>
+.signup-form {
+    margin: 20px;
+    padding: 40px;
+}
+.signup-form .submit-btn {
+    display: block;
+    margin: auto;
+}
+</style>
