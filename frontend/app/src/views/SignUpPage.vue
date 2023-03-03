153e3a6685fcf9c4c9ce6c3f355232bb228e1784
about-yy <work.yamaoka@gmail.com>
2023-03-03 15:03:09 +0900

    自動ログイン処理を追加

--- frontend/app/src/views/SignUpPage.vue
+++ frontend/app/src/views/SignUpPage.vue
@@ -1,13 +1,13 @@
 <template>
     <v-card class="signup-form" title="ユーザ登録 | TS TODO APP">
         <v-container>
-            <ErrorMessage v-if="loginFormState.isFailed" message="ユーザ登録に失敗しました。入力内容を確認してください。"></ErrorMessage>
-            <v-form v-model="loginFormState.form" @submit.prevent="onSubmit">
-                <v-text-field label="メールアドレス" v-model="loginForm.email" id="email" :readonly="loginFormState.loading" :rules="[requiredValidation]" clearable class="text-input" ></v-text-field>
-                <v-text-field label="ユーザー名" v-model="loginForm.username" id="username" :readonly="loginFormState.loading" :rules="[requiredValidation]" clearable class="text-input" ></v-text-field>
-                <v-text-field label="パスワード" v-model="loginForm.password" id="password" :readonly="loginFormState.loading" :rules="[requiredValidation]" clearable type="password" class="text-input" ></v-text-field>
-                <v-text-field label="パスワード（確認用）" v-model="loginForm.password_confirm" id="password_confirm" :readonly="loginFormState.loading" :rules="[requiredValidation]" clearable type="password" class="text-input" ></v-text-field>
-                <v-btn type="submit" class="submit-btn" :loading="loginFormState.loading" :disabled="!loginFormState.form" variant="flat" color="primary" >登録</v-btn>
+            <ErrorMessage v-if="signupFormState.isFailed" message="ユーザ登録に失敗しました。入力内容を確認してください。"></ErrorMessage>
+            <v-form v-model="signupFormState.form" @submit.prevent="onSubmit">
+                <v-text-field label="メールアドレス" v-model="signupForm.email" id="email" :readonly="signupFormState.loading" :rules="[requiredValidation]" clearable class="text-input" ></v-text-field>
+                <v-text-field label="ユーザー名" v-model="signupForm.username" id="username" :readonly="signupFormState.loading" :rules="[requiredValidation]" clearable class="text-input" ></v-text-field>
+                <v-text-field label="パスワード" v-model="signupForm.password" id="password" :readonly="signupFormState.loading" :rules="[requiredValidation]" clearable type="password" class="text-input" ></v-text-field>
+                <v-text-field label="パスワード（確認用）" v-model="signupForm.password_confirm" id="password_confirm" :readonly="signupFormState.loading" :rules="[requiredValidation]" clearable type="password" class="text-input" ></v-text-field>
+                <v-btn type="submit" class="submit-btn" :loading="signupFormState.loading" :disabled="!signupFormState.form" variant="flat" color="primary" >登録</v-btn>
             </v-form>
         </v-container>
     </v-card>
@@ -17,6 +17,8 @@ import { defineComponent, reactive } from 'vue'
 import { useRouter } from 'vue-router';
 import AxiosUtil from '../utils/AxiosUtil';
 import ErrorMessage from '../components/ErrorMessage.vue';
+import { useStore } from '../store';
+import * as ActionTypes from '../store/ActionTypes';
 
 export default defineComponent({
     components: {
@@ -24,45 +26,55 @@ export default defineComponent({
     },  
     setup() {
         const router = useRouter();
-
-        const loginForm = reactive({
+        const store = useStore();
+        const signupForm = reactive({
             email: "",
             username: "",
             password: "",
             password_confirm: "",
         });
-        const loginFormState = reactive({
+        const signupFormState = reactive({
             loading: false,
             form: false,
             isFailed: false,
         });
+
         const onSubmit = async ()=>{
-            if(!loginFormState.form) return;
-            if(loginForm.password !== loginForm.password_confirm) return;
-            loginFormState.loading = true;
+            if(!signupFormState.form) return;
+            if(signupForm.password !== signupForm.password_confirm) return;
+            signupFormState.loading = true;
 
-            const result = AxiosUtil.post("/user/regist", {
-                email: loginForm.email,
-                username: loginForm.username,
-                password: loginForm.password
-            }).then((res)=>{
-                // タスクページへ遷移
-                if(res.status === 200){
-                    loginFormState.isFailed = false;
+            signup(signupForm.email, signupForm.username, signupForm.password)
+                .then((res)=>login(signupForm.email, signupForm.password))
+                .then((res)=>{
+                    signupFormState.isFailed = false
                     moveToTaskPage();
-                } else {
-                    // エラーメッセージを表示
-                    loginFormState.isFailed = true;
-                }
-            }).catch(error=>{
-                // エラーメッセージを表示
-                loginFormState.isFailed = true;
-            }).finally(()=>{
-                // 状態を初期化
-                loginFormState.loading = false;
-            })
-
+                })
+                .catch((e)=>{signupFormState.isFailed = true})
+                .finally(()=>{signupFormState.loading = false});
+        
         }
+        const signup = async(email: string, username: string, password: string)=>{
+            const result = await AxiosUtil.post("/user/regist", {
+                email: email,
+                username: username,
+                password: password
+            })
+            return result;
+        }
+
+        const login = async(email: string, password: string)=>{
+            const result = await AxiosUtil.post("/auth/login", {
+                email: email,
+                password: password
+            }).then((res)=>{
+                console.log(res.headers);
+                console.log(res.headers.getAuthorization);
+                store.dispatch(ActionTypes.SET_ACCESS_TOKEN, res.headers.authorization);
+            })
+            return result;
+        }
+
         const moveToTaskPage = ()=>{
             router.push({name: "app"});
         }
@@ -70,8 +82,8 @@ export default defineComponent({
             return !!value||"required";
         }
         return {
-            loginForm,
-            loginFormState,
+            signupForm,
+            signupFormState,
             requiredValidation,
             onSubmit
         }
