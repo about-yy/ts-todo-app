fb3a25d4fcdb7f405a135d754872bc69be0298c1
about-yy <work.yamaoka@gmail.com>
2023-03-04 11:01:28 +0900

    タスク取得処理を追加

--- frontend/app/src/views/TaskPage.vue
+++ frontend/app/src/views/TaskPage.vue
@@ -1,15 +1,28 @@
 <template>
-    <p>hello from task page</p>
+    <p>{{ token }}</p>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent, onBeforeMount } from 'vue'
+import { useStore } from '../store'
+import AxiosUtil from '../utils/AxiosUtil'
 
 export default defineComponent({
     setup() {
-        
+        const getTasks = async ()=>{
+            const result = await AxiosUtil.get('/task/list')
+            console.log(result);
+        }
+        onBeforeMount(getTasks);
+        const store = useStore();
+        const token = computed(()=>store.state.token);
+        return {
+            token
+        }
     },
 })
 </script>
 <style scoped>
-
+.taskItem:hover {
+    background-color: aliceblue;
+}
 </style>
