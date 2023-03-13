178b121d20f64b6464bca887d9b0486601c2aca5
about-yy <work.yamaoka@gmail.com>
2023-03-13 19:07:54 +0900

    vuetifyを削除

--- frontend/app/src/main.ts
+++ frontend/app/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './app/router'
 import { key, store } from './app/store'
-import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import 'v-calendar/dist/style.css';
 loadFonts()
@@ -10,5 +9,4 @@ loadFonts()
 createApp(App)
   .use(router)
   .use(store, key)
-  .use(vuetify)
   .mount('#app')
