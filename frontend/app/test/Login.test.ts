ec60ddcf94e5bca124a1c2555b9886659a198111
about-yy <work.yamaoka@gmail.com>
2023-03-16 17:09:03 +0900

    テストコードを変更

--- frontend/app/test/Login.test.ts
+++ frontend/app/test/Login.test.ts
@@ -1,5 +1,5 @@
 import { render, fireEvent } from "@testing-library/vue";
-import { Mocked, vi } from "vitest";
+import { expect, Mocked, vi } from "vitest";
 import LoginForm from "../src/views/LoginPage.vue";
 import "@testing-library/jest-dom";
 import axios from "axios";
@@ -12,23 +12,7 @@ describe("LoginForm", () => {
     mockedAxios.post.mockReset();
   });
 
-  it("フォームが空のまま送信された場合、エラーメッセージが表示される", async () => {
-    const { getByRole, findByText } = render(LoginForm);
-    // 空のフォームを送信
-    const form = getByRole("form");
-    expect(form).toBeTruthy();
-    await fireEvent.submit(form);
-    expect(
-      await findByText(
-        "メールアドレス、またはパスワードに誤りがあります。入力内容を確認してください。"
-      )
-    ).toBeInTheDocument();
-
-    // フォームが実行されたことを確認
-    expect(mockedAxios.post).toHaveBeenCalledTimes(1);
-  });
-
-  it("メールアドレスとパスワードが有効な場合、フォームが送信される", async () => {
+  it("ログインできる", async () => {
     const { getByLabelText, getByRole } = render(LoginForm);
 
     // メールアドレスとパスワードを入力
@@ -43,5 +27,47 @@ describe("LoginForm", () => {
 
     // フォームが実行されたことを確認
     expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+  });
+
+  it("フォームが空の場合、ログインできない", async () => {
+    const { getByRole, findByText } = render(LoginForm);
+
+    // フォームを送信
+    const form = getByRole("form");
+    await fireEvent.submit(form);
+
+    // フォームが実行されたことを確認
+    expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+
+    // エラーメッセージが表示されることを確認
+    expect(
+      await findByText(
+        "メールアドレス、またはパスワードに誤りがあります。入力内容を確認してください。"
+      )
+    ).toBeInTheDocument();
+  });
+
+  it("フォームに不正な値が入力されている場合、ログインできない", async () => {
+    const { getByLabelText, getByRole, findByText } = render(LoginForm);
+
+    // メールアドレスとパスワードを入力
+    const emailInput = getByLabelText("メールアドレス");
+    const passwordInput = getByLabelText("パスワード");
+    await fireEvent.update(emailInput, "example.com");
+    await fireEvent.update(passwordInput, "password");
+
+    // フォームを送信
+    const form = getByRole("form");
+    await fireEvent.submit(form);
+
+    // フォームが実行されたことを確認
+    expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+
+    // エラーメッセージが表示されることを確認
+    expect(
+      await findByText(
+        "メールアドレス、またはパスワードに誤りがあります。入力内容を確認してください。"
+      )
+    ).toBeInTheDocument();
   });
 });
