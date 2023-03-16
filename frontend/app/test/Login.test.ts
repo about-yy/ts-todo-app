44fcdde5f41ceec9f01ebe51512075779f435d12
about-yy <work.yamaoka@gmail.com>
2023-03-16 15:46:04 +0900

    テストコードを修正

--- frontend/app/test/Login.test.ts
+++ frontend/app/test/Login.test.ts
@@ -3,12 +3,18 @@ import { Mocked, vi } from "vitest";
 import LoginForm from "../src/views/LoginPage.vue";
 import "@testing-library/jest-dom";
 import axios from "axios";
+vi.mock("axios");
+
+const mockedAxios = axios as Mocked<typeof axios>;
 
 describe("LoginForm", () => {
+  beforeEach(() => {
+    mockedAxios.post.mockReset();
+  });
+
   it("フォームが空のまま送信された場合、エラーメッセージが表示される", async () => {
     const { getByRole, findByText } = render(LoginForm);
-
-    // Submit form with empty fields
+    // 空のフォームを送信
     const form = getByRole("form");
     expect(form).toBeTruthy();
     await fireEvent.submit(form);
@@ -17,24 +23,25 @@ describe("LoginForm", () => {
         "メールアドレス、またはパスワードに誤りがあります。入力内容を確認してください。"
       )
     ).toBeInTheDocument();
+
+    // フォームが実行されたことを確認
+    expect(mockedAxios.post).toHaveBeenCalledTimes(1);
   });
 
   it("メールアドレスとパスワードが有効な場合、フォームが送信される", async () => {
     const { getByLabelText, getByRole } = render(LoginForm);
 
-    // Enter email and password
+    // メールアドレスとパスワードを入力
     const emailInput = getByLabelText("メールアドレス");
     const passwordInput = getByLabelText("パスワード");
     await fireEvent.update(emailInput, "test@example.com");
     await fireEvent.update(passwordInput, "password");
 
-    // Submit form
+    // フォームを送信
     const form = getByRole("form");
     await fireEvent.submit(form);
 
-    // Check that form was submitted
-    vi.mock("axios");
-    const mockedAxios = axios as Mocked<typeof axios>;
-    expect(mockedAxios.post).toHaveBeenCalledTimes(2);
+    // フォームが実行されたことを確認
+    expect(mockedAxios.post).toHaveBeenCalledTimes(1);
   });
 });
