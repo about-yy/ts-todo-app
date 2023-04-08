6b0f830c2fe866a2c0bc0513d8372b5d45ef63cf
about-yy <work.yamaoka@gmail.com>
2023-04-08 20:55:18 +0900

    見やすく整形

--- backend/app/test/auth/AuthIsLogined.spec.ts
+++ backend/app/test/auth/AuthIsLogined.spec.ts
@@ -4,91 +4,80 @@ import index from "../../src/index";
 import testdata from "./authIsLogined.testdata.json";
 
 const app = supertest(index);
-describe("ログインチェック機能のテスト", async()=>{
-    it("正常処理", async()=>{
-        const user = testdata.users.correct_token;
-        const registResult = await app
-            .post("/user/regist")
-            .send({
-                email: user.email,
-                password: user.password,
-                username: user.username
-            });
-        expect(registResult.status).to.eq(200);
-
-        const loginResult = await app
-            .post("/auth/login")
-            .send({
-                email: user.email,
-                password: user.password
-            });
-        expect(loginResult.status).to.eq(200);
-
-        const isLoginResult = await app
-            .get("/auth/isLogined")
-            .set("authorization", loginResult.headers.authorization);
-        expect(isLoginResult.status).to.eq(200);
-        expect(isLoginResult.body).has.keys(["result"]);
-        expect(isLoginResult.body.result).to.eq(true);
-
+describe("ログインチェック機能のテスト", async () => {
+  it("正常処理", async () => {
+    const user = testdata.users.correct_token;
+    const registResult = await app.post("/user/regist").send({
+      email: user.email,
+      password: user.password,
+      username: user.username,
     });
-    it("トークン無し", async()=>{
-        const user = testdata.users.no_token;
-        const registResult = await app
-            .post("/user/regist")
-            .send({
-                email: user.email,
-                password: user.password,
-                username: user.username
-            });
-        expect(registResult.status).to.eq(200);
+    expect(registResult.status).to.eq(200);
 
-        const loginResult = await app
-            .post("/auth/login")
-            .send({
-                email: user.email,
-                password: user.password
-            });
-        expect(loginResult.status).to.eq(200);
-
-        const isLoginResult = await app
-            .get("/auth/isLogined");
-
-        expect(isLoginResult.status).to.eq(401);
-        expect(isLoginResult.body).include.keys(["result"]);
-        expect(isLoginResult.body.result).to.eq(false);
-    })
-
-    it("不正なトークン", async()=>{
-        const user = testdata.users.invalid_token;
-        const registResult = await app
-            .post("/user/regist")
-            .send({
-                email: user.email,
-                password: user.password,
-                username: user.username
-            });
-        expect(registResult.status).to.eq(200);
-
-        const loginResult = await app
-            .post("/auth/login")
-            .send({
-                email: user.email,
-                password: user.password
-            });
-        expect(loginResult.status).to.eq(200);
-
-        const isLoginResult = await app
-            .get("/auth/isLogined")
-            .set("authorization", loginResult.headers.authorization.split("").reverse().join(""));
-
-        expect(isLoginResult.status).to.eq(401);
-        expect(isLoginResult.body).include.keys(["result"]);
-        expect(isLoginResult.body.result).to.eq(false);
+    const loginResult = await app.post("/auth/login").send({
+      email: user.email,
+      password: user.password,
     });
+    expect(loginResult.status).to.eq(200);
 
-    // it("トークンの期限切れ", async (){
-    //     @todo
-    // });
+    const isLoginResult = await app
+      .get("/auth/isLogined")
+      .set("authorization", loginResult.headers.authorization);
+    expect(isLoginResult.status).to.eq(200);
+    expect(isLoginResult.body).has.keys(["result"]);
+    expect(isLoginResult.body.result).to.eq(true);
+  });
 
+  it("トークン無し", async () => {
+    const user = testdata.users.no_token;
+    const registResult = await app.post("/user/regist").send({
+      email: user.email,
+      password: user.password,
+      username: user.username,
+    });
+    expect(registResult.status).to.eq(200);
+
+    const loginResult = await app.post("/auth/login").send({
+      email: user.email,
+      password: user.password,
+    });
+    expect(loginResult.status).to.eq(200);
+
+    const isLoginResult = await app.get("/auth/isLogined");
+
+    expect(isLoginResult.status).to.eq(401);
+    expect(isLoginResult.body).include.keys(["result"]);
+    expect(isLoginResult.body.result).to.eq(false);
+  });
+
+  it("不正なトークン", async () => {
+    const user = testdata.users.invalid_token;
+    const registResult = await app.post("/user/regist").send({
+      email: user.email,
+      password: user.password,
+      username: user.username,
+    });
+    expect(registResult.status).to.eq(200);
+
+    const loginResult = await app.post("/auth/login").send({
+      email: user.email,
+      password: user.password,
+    });
+    expect(loginResult.status).to.eq(200);
+
+    const isLoginResult = await app
+      .get("/auth/isLogined")
+      .set(
+        "authorization",
+        loginResult.headers.authorization.split("").reverse().join("")
+      );
+
+    expect(isLoginResult.status).to.eq(401);
+    expect(isLoginResult.body).include.keys(["result"]);
+    expect(isLoginResult.body.result).to.eq(false);
+  });
+
+  // it("トークンの期限切れ", async (){
+  //     @todo
+  // });
 });
